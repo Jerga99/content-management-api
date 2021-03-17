f95c46c0e9f75f3be96af07d5823ed9e648e4015
Filip Jerga <jerga99@gmail.com>
2021-03-17 09:09:40 +0000

    Get Resources from api

--- index.js
+++ index.js
@@ -3,12 +3,19 @@ const express = require("express");
 const app = express();
 const PORT = 3001;
 
+const fs = require("fs");
+const path = require("path");
+const pathToFile = path.resolve("./data.json");
+
+const getResources = () => JSON.parse(fs.readFileSync(pathToFile))
+
 app.get("/", (req, res) => {
   res.send("Hello World")
 })
 
 app.get("/api/resources", (req, res) => {
-  res.send("Hello Resources")
+  const resources = getResources();
+  res.send(resources);
 })
 
 app.listen(PORT, () => {
