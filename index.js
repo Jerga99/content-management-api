71903a526ac867ce18951e62ec2411428e4eb8f6
Filip Jerga <jerga99@gmail.com>
2021-03-18 09:21:13 +0000

    Finishing POST endpoint

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const pathToFile = path.resolve("./data.json");
 
 const getResources = () => JSON.parse(fs.readFileSync(pathToFile))
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello World")
 })
@@ -17,6 +19,25 @@ app.get("/api/resources", (req, res) => {
   const resources = getResources();
   res.send(resources);
 })
+
+app.post("/api/resources", (req, res) => {
+  const resources = getResources();
+  const resource = req.body;
+
+  resource.createdAt = new Date();
+  resource.status = "inactive";
+  resource.id = Date.now().toString();
+  resources.unshift(resource);
+
+  fs.writeFile(pathToFile, JSON.stringify(resources, null, 2), (error) => {
+    if (error) {
+      return res.status(422).send("Cannot store data in the file!");
+    }
+
+    return res.send("Data has been saved!");
+  })
+})
+
 
 app.listen(PORT, () => {
   console.log("Server is listening on port:" + PORT);
